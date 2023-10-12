cf5998f7a3c2d48894660adc75f364fd3edf113f
raneemhamarneh <raneem99hamarneh@gmail.com>
2023-10-12 11:30:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+
+const names = [
+    "Guadalupe", 
+    "Ollie", 
+    "Aki"]
+
+function writeCards(names){
+
+    const messages = [];
+    for (let i=0 ; i < names.length; i++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return messages;
+}
+function countDown(num){
+    for (let i = num; i >= 0; i--) {
+        console.log(i);
+    }
+}
+
+console.log(writeCards(names));
+countDown(5);
+
